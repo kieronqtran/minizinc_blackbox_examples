04351f2d66553d2d49f1c237629c23db23367818
Tran Huu Quang <kieron.qtran@gmail.com>
2024-07-30 18:45:11 +1000

    Update the python code to adopt Jip new pipeline

--- bb_gini_py/bb_gini.py
+++ bb_gini_py/bb_gini.py
@@ -12,14 +12,22 @@ if __name__ == "__main__":
   # Continuously read lines from stdin until the pipe is closed
   for line in stdin:
     # Parse the input line
-    parts = line.split('E')
-    assert len(parts) == 3, f"Expected 2 occurences of E in the input, but split into {parts}"
-    assert parts[2] in ["", "\n"], f"No input expected on the same line after the last 'E', but found the following data `{parts[2]}'"
+    parts = line.split(";")
+    assert (
+        len(parts) == 3
+    ), f"Expected 2 occurences of E in the input, but split into {parts}"
+    assert parts[2] in [
+        "",
+        "\n",
+    ], f"No input expected on the same line after the last 'E', but found the following data `{parts[2]}'"
     integers = [int(i) for i in parts[0].split()]
     floats = [float(f) for f in parts[1].split()]
 
     # Call the (now not so) blackbox function
-    (int_out, float_out) = bb_gini(integers, floats)
+    (int_out, float_out) = bb_simulator(integers, floats)
 
     # Communicate output to encapsulating process
-    print(f"{' '.join([str(i) for i in int_out])} E {' '.join([str(f) for f in float_out])} E", flush=True)
+    print(
+        f"{' '.join([str(i) for i in int_out])}; {' '.join([str(f) for f in float_out])};",
+        flush=True,
+    )
