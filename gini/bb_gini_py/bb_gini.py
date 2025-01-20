13871c20cbd16e231b6d22076f2e359d5115ef52
Nathan Tran <kieron.qtran@gmail.com>
2025-01-20 20:32:06 +1100

    Clean up unrelated files

--- gini/bb_gini_py/bb_gini.py
+++ gini/bb_gini_py/bb_gini.py
@@ -6,28 +6,29 @@ from typing import List, Tuple
 from gini_np import gini
 
 def bb_gini(integers: List[int], floats: List[float]) -> Tuple[List[int], List[float]]:
-  return ([gini(integers) * 100], [])
+  scale, *values = integers
+  return ([int(gini(values) * scale)], [])
 
 if __name__ == "__main__":
   # Continuously read lines from stdin until the pipe is closed
   for line in stdin:
-    # Parse the input line
-    parts = line.split(";")
-    assert (
-        len(parts) == 3
-    ), f"Expected 2 occurences of E in the input, but split into {parts}"
-    assert parts[2] in [
-        "",
-        "\n",
-    ], f"No input expected on the same line after the last 'E', but found the following data `{parts[2]}'"
-    integers = [int(i) for i in parts[0].split()]
-    floats = [float(f) for f in parts[1].split()]
+      # Parse the input line
+      parts = line.split(";")
+      assert (
+          len(parts) == 3
+      ), f"Expected 2 occurences of E in the input, but split into {parts}"
+      assert parts[2] in [
+          "",
+          "\n",
+      ], f"No input expected on the same line after the last 'E', but found the following data `{parts[2]}'"
+      integers = [int(i) for i in parts[0].split()]
+      floats = [float(f) for f in parts[1].split()]
 
-    # Call the (now not so) blackbox function
-    int_out, float_out = bb_gini(integers, floats)
+      # Call the (now not so) blackbox function
+      (int_out, float_out) = bb_gini(integers, floats)
 
-    # Communicate output to encapsulating process
-    print(
-        f"{' '.join([str(i) for i in int_out])}; {' '.join([str(f) for f in float_out])};",
-        flush=True,
-    )
+      # Communicate output to encapsulating process
+      print(
+          f"{' '.join([str(i) for i in int_out])}; {' '.join([str(f) for f in float_out])};",
+          flush=True,
+      )
--- gini/bb_gini_py/gini.py
+++ gini/bb_gini_py/gini.py
@@ -1,8 +1,7 @@
 from typing import List
 
 def gini(x: List[int]) -> float:
-  x_clone = x.copy()
-  sorted(x_clone)
+  x_clone = sorted(x)
   n = len(x_clone)
   diffs = sum((2 * i - n - 1) * xi for i, xi in enumerate(x_clone, 1))
   return diffs / (n * sum(x_clone))
