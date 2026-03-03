8c738b895a0a4983557f0d6b7b203fa7a24381b9
Nathan Tran <kieron.qtran@gmail.com>
2026-03-03 11:47:06 +0700

    Adding Result for Windfarm07 with fix 1 thread and 25 restart_scales

--- benchmarks/wind/start_bench_windfarm06.py
+++ benchmarks/wind/start_bench_windfarm06.py
@@ -16,21 +16,21 @@ driver = Driver(Path(os.getenv("MINIZINC_PATH", default=shutil.which("minizinc")
 current_dir = Path(__file__).parent
 #%%
 schedule(
-    instances=Path("./instances.csv"),
+    instances=Path("./instances_windfarm06.csv"),
     timeout=timedelta(minutes=int(os.getenv("TIMEOUT_MIN", default=15))),
     configurations=[
-        Configuration(name=f"Gecode_Windfarm06_{i}_{j}",
+        Configuration(name=f"Gecode_Windfarm06_{per}_{scale}_{thread}",
                       solver=Solver.lookup("gecode", driver=driver),
                       other_flags={
                         "--param-file": str(current_dir / "models" / "configs" / "gecode_bb.mpc"),
-                        "--restart-scale": j,
+                        "--restart-scale": scale,
                         "--restart": "constant",
+                        "--parallel": thread,
                       },
                       extra_data={
-                        "initTemp": 10000,
-                        "coolingRate": j,
+                        "percentage": per,
                       }
         )
-        for i, j in product(range(0.005, 1, 0.005), [1] + list(range(25, 251, 25)))
+        for per, scale, thread in product([-1] + list(range(75, 100, 5)), [1] + list(range(25, 251, 25), range(1, 10)))
     ],
 )
